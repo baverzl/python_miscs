d72febfffdf6c579f5a3979a1e4764f800a77a78
baverzl <baverzl.doer@gmail.com>
2019-03-20 00:26:04 +0900

    Update functools_partial.py

--- functools_ex/functools_partial.py
+++ functools_ex/functools_partial.py
@@ -13,3 +13,28 @@ basetwo = partial(int, base=2)
 basetwo.__doc__ = 'Convert base 2 string to an int.'
 print( basetwo('10010') )
 
+def power(base, exponent):
+    return base ** exponent
+
+def square(base):
+    return power(base, 2)
+
+def cube(base):
+    return power(base, 3)
+
+square = partial(power, exponent=2)
+cube = partial(power, exponent=3)
+
+def test_partials():
+    assert square(2) == 4
+    assert cube(2) == 8
+
+def test_partial_docs():
+    assert square.keywords == {"exponent": 2}
+    assert square.func == power
+
+    assert cube.keywords == {"exponent": 3}
+    assert cube.func == power
+
+test_partials()
+test_partial_docs()
