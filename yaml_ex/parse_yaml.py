7d42cf0f6291a14c8752e32c2a394c3984357ea9
baverzl <baverzl.doer@gmail.com>
2019-03-20 01:57:37 +0900

    Add yaml examples

---
+++ yaml_ex/parse_yaml.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python2
+
+import yaml
+
+with open('config.yaml', 'r') as ymlfile:
+    cfg = yaml.load(ymlfile)
+
+for section in cfg:
+    print(section)
+
+print(cfg['mysql'])
+print(cfg['other'])
